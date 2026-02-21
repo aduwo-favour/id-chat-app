8d83cb1cd3f52bd65b11ba114f690e80f56af231
aduwo-favour <77938463+aduwo-favour@users.noreply.github.com>
2026-02-21 13:38:29 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBEPEEQR63z_Dym50j3mS46ZyzPgMLbsi0",
+  authDomain: "chat-messaging-abaa9.firebaseapp.com",
+  projectId: "chat-messaging-abaa9",
+  appId: "1:625429860180:web:6719187a4eaa0be53d82c1"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
