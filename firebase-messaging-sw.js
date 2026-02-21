fc32ddafe089e39071c8783ef3e462a9240cd3ca
aduwo-favour <77938463+aduwo-favour@users.noreply.github.com>
2026-02-21 14:44:25 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,11 +2,10 @@ importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBEPEEQR63z_Dym50j3mS46ZyzPgMLbsi0",
+  authDomain: "chat-messaging-abaa9.firebaseapp.com",
+  projectId: "chat-messaging-abaa9",
+  appId: "1:625429860180:web:6719187a4eaa0be53d82c1"
 });
 
 const messaging = firebase.messaging();
