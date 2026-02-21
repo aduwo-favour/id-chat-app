b6696806ffdb4be97f5a95ce1c7a1fad9e718d8f
aduwo-favour <77938463+aduwo-favour@users.noreply.github.com>
2026-02-21 16:00:07 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+import { 
+  getAuth 
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+
+import { 
+  getFirestore,
+  enableIndexedDbPersistence
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+/* ================= FIREBASE CONFIG ================= */
 
 const firebaseConfig = {
   apiKey: "AIzaSyBEPEEQR63z_Dym50j3mS46ZyzPgMLbsi0",
@@ -9,7 +18,19 @@ const firebaseConfig = {
   appId: "1:625429860180:web:6719187a4eaa0be53d82c1"
 };
 
+/* ================= INITIALIZE ================= */
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+/* ================= ENABLE OFFLINE CACHE ================= */
+
+enableIndexedDbPersistence(db, { synchronizeTabs: true })
+  .then(() => {
+    console.log("🔥 Firestore offline cache enabled");
+  })
+  .catch((err) => {
+    console.log("Cache error:", err.code);
+  });
