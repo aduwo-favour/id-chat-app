1c39dd85b93e513b8fcb93b518d5bb12b35abf4f
aduwo-favour <77938463+aduwo-favour@users.noreply.github.com>
2026-02-24 12:00:19 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,6 @@
-import { initializeApp } from 
-"https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-
-import { getAuth } from 
-"https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-
-import { 
-  getFirestore, 
-  enableIndexedDbPersistence,
-  connectFirestoreEmulator
-} from 
-"https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-
-/* ================= FIREBASE CONFIG ================= */
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBEPEEQR63z_Dym50j3mS46ZyzPgMLbsi0",
@@ -21,12 +10,6 @@ const firebaseConfig = {
   appId: "1:625429860180:web:6719187a4eaa0be53d82c1"
 };
 
-/* ================= INITIALIZE ================= */
-
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Don't enable persistence immediately - it can cause issues
-// We'll enable it after auth is working
